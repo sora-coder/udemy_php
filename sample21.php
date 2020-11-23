a1035d3fad5174ea0e256c18e29914b0479a4f20
sora-coder <rqqgx271@yahoo.co.jp>
2020-11-23 12:40:27 +0900

    正規表現・全角英数字を半角に変換

---
+++ sample21.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>
+</header>
+<main>
+<h2>Practice</h2>
+<pre>
+<?php
+    $zip = '１２３-４４４４';
+    $zip = mb_convert_kana($zip, 'a', 'UTF-8');
+    if(preg_match("/\A\d{3}[-]\d{4}\z/", $zip)){
+        echo('郵便番号：〒' . $zip);
+    }else{
+        echo('郵便番号を123−5678の形式で入力してください');
+    }
+?>
+
+</pre>
+</main>
+</body>
+</html>
