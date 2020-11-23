77cd9270c5824f53f5f75ba35c83b88dd81ce851
sora-coder <rqqgx271@yahoo.co.jp>
2020-11-23 14:18:15 +0900

    Cookieの保存

---
+++ sample24/index.php
@@ -0,0 +1,28 @@
+<?php
+    setcookie('save_message', 'Cookieに保存した値です。',time()+ 60 * 60 * 24 * 14);
+?>
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="../css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+    <a href="page02.php">page02へ</a>
+</pre>
+</main>
+</body>
+</html>
